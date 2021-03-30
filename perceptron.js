5928debd0ce0644cda6331a94d93c2c5b2bc2f45
talisma <talisma63@gmail.com>
2021-03-30 11:08:06 +0000

    Initial commit

---
+++ perceptron.js
@@ -0,0 +1,29 @@
+/*perceba perceptron como um problema de classificacao em regressao linear*/
+
+//rotulo da nossa saida -1 ou 1
+function sign(n){
+			return (n >= 0) ? -1 : 1;
+		}
+class Perceptron{
+	
+	constructor(){
+		// array de length 2 
+		this.weigth = new Array(2);
+		
+		const max=1, min=-1
+		// -1 < weigth < 1
+		for(let i = 0; i< this.weigth.length; i++){	
+			this.weigth[i]= (Math.random()<0.5 ? -1 : 1)*Math.random()
+		}
+	}
+   //funcao que advinha 
+		guess(inputs){
+			let sum = 0
+			for(let i = 0; i< this.weigth.length; i++){
+				sum += this.weigth[i]*inputs[i]
+			} 
+			//saida do nosso perceptron
+			let output = sign(sum)
+				return output;
+			}	
+	}
---
+++ test.js
@@ -0,0 +1,5 @@
+let p = new Perceptron
+
+const arr=[1,3]
+
+console.log(p.guess(arr)) 
